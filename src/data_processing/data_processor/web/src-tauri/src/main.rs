5e49f7feb96cf6be62253ca61fa94a430b4bce21
dieterolson <dieterolson@gmail.com>
2026-02-26 19:41:01 -0800

    fix(ci): apply rustfmt to tauri app sources

--- src/data_processing/data_processor/web/src-tauri/src/main.rs
+++ src/data_processing/data_processor/web/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  data_processor_lib::run();
+    data_processor_lib::run();
 }
--- src/function_generator/web/src-tauri/src/main.rs
+++ src/function_generator/web/src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  function_generator_lib::run();
+    function_generator_lib::run();
 }
