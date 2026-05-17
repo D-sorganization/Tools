c1a0cd51356285561c5f1ef23297f1aaf8c81576
Dieter Olson <dieterolson@gmail.com>
2026-05-17 15:01:23 -0700

    fix(ui): allow sidekick tabs to shrink

--- src/shared/python/sidekick/ui/tools_sidebar/sidebar.py
+++ src/shared/python/sidekick/ui/tools_sidebar/sidebar.py
@@ -161,6 +161,8 @@ class UnifiedToolsSidebar(
         self.tabs.setObjectName(SIDEKICK_TABS_OBJECT_NAME)
         self.tabs.tabBar().setObjectName(SIDEKICK_TAB_BAR_OBJECT_NAME)
         self.tabs.setMovable(True)
+        self.tabs.setUsesScrollButtons(True)
+        self.tabs.tabBar().setElideMode(QtCore.Qt.TextElideMode.ElideRight)
         self.tabs.currentChanged.connect(self._emit_context)
         self.tabs.tabBar().tabMoved.connect(self._sync_tab_order_from_widget)
 
